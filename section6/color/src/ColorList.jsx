c6969f9abdbf1b5fe2809dbf666c20b670c0451d
Matsuda Takuya <matsuda.takuya41@gmail.com>
2022-03-07 23:17:06 +0900

    commit

---
+++ section6/color/src/ColorList.jsx
@@ -0,0 +1,21 @@
+import { Color } from "./Color.jsx";
+
+export const ColorList = ({
+  colors = [],
+  onRemoveColor = (f) => f,
+  onRateColor = (f) => f,
+}) => {
+  if (!colors.length) return <div>No Colors Listed.</div>;
+  return (
+    <div>
+      {colors.map((color) => (
+        <Color
+          key={color.id}
+          {...color}
+          onRemove={onRemoveColor}
+          onRate={onRateColor}
+        />
+      ))}
+    </div>
+  );
+};
